9bcd76e1f975fc7f7da60a8f8ce8f3fa805c7c50
jittuganissy <24B11DS079@adityauniversity.in>
2025-10-15 10:13:35 +0530

    Add files via upload

---
+++ 5.2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void greet(string name = "Guest") {
+    cout << "Hello, " << name << "!" << endl;
+}
+
+int main() {
+    greet();              // Uses default argument "Guest"
+    greet("Madhuri");    // Uses provided argument
+    return 0;
+}
